2d5d7d8046cbf72dc0f158ce72c1613336ab5376
Philippe Creux <pcreux@gmail.com>
2017-03-31 14:56:54 -0700

    Create gem

---
+++ lib/locate/images.rb
@@ -0,0 +1,7 @@
+require "locate/images/version"
+
+module Locate
+  module Images
+    # Your code goes here...
+  end
+end
---
+++ lib/locate/images/version.rb
@@ -0,0 +1,5 @@
+module Locate
+  module Images
+    VERSION = "0.1.0"
+  end
+end
