629090d1f0eff56044bd0c1b02d4232c55f9662d
Philippe Creux <pcreux@gmail.com>
2017-03-31 16:10:24 -0700

    Refactor cli.rb

--- lib/locate_images/cli.rb
+++ lib/locate_images/cli.rb
@@ -3,30 +3,44 @@ require 'optparse'
 module LocateImages
   class CLI
     def self.run
-      directory = ARGV[0] || "."
-      format = "csv"
-      output = $stdout
+      new.call
+    end
 
+    def call
+      initialize_default_options
+      parse_options
+      run
+    end
+
+    def initialize_default_options
+      @directory = ARGV[0] || "."
+      @format = "csv"
+      @output = $stdout
+    end
+
+    def parse_options
       OptionParser.new do |opts|
         opts.banner = "Usage: example.rb [options]"
 
         opts.on("-f", "--format FORMAT", ["csv", "html"], "csv by default. Accepts `csv` or `html`") do |f|
-          format = f
+          @format = f
         end
 
         opts.on("-o", "--output FILE", "stdout by default") do |f|
-          output = File.open(f, 'w')
+          @output = File.open(f, 'w')
         end
       end.parse!
+    end
 
-      formatter = case format
-      when "csv"
-        GenerateCSV
-      when "html"
-        GenerateHTML
-      end
+    def run
+      formatter.call(image_paths: ListImages.call(directory: @directory), output: @output)
+    end
 
-      formatter.call(image_paths: ListImages.call(directory: directory), output: output)
+    def formatter
+      {
+        "csv" => GenerateCSV,
+        "html" => GenerateHTML
+      }.fetch(@format)
     end
   end
 end
