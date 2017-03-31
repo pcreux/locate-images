1832f6d82d54c0aeffe722a4e42c1b02f9fb069d
Philippe Creux <pcreux@gmail.com>
2017-03-31 16:06:36 -0700

    Add -o --output

--- lib/locate_images/cli.rb
+++ lib/locate_images/cli.rb
@@ -5,12 +5,17 @@ module LocateImages
     def self.run
       directory = ARGV[0] || "."
       format = "csv"
+      output = $stdout
 
       OptionParser.new do |opts|
         opts.banner = "Usage: example.rb [options]"
 
         opts.on("-f", "--format FORMAT", ["csv", "html"], "csv by default. Accepts `csv` or `html`") do |f|
           format = f
+        end
+
+        opts.on("-o", "--output FILE", "stdout by default") do |f|
+          output = File.open(f, 'w')
         end
       end.parse!
 
@@ -21,7 +26,7 @@ module LocateImages
         GenerateHTML
       end
 
-      puts formatter.call(image_paths: ListImages.call(directory: directory))
+      formatter.call(image_paths: ListImages.call(directory: directory), output: output)
     end
   end
 end
